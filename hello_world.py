16104b509799a7078232a545e627d194011d4105
VadimBir <50209845+VadimBir@users.noreply.github.com>
2023-07-23 17:17:48 -0500

    Hel World

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi():
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(print_hi())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
